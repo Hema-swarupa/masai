69f403c065742c363a9e15f4dfe1df604e646594
HEMA-SWARUPA <hemaswarupagondela@gmail.com>
2025-11-26 22:47:36 +0530

    second

---
+++ closures/q19/1.js
@@ -0,0 +1,14 @@
+var age = 20;
+
+function displayAge() {
+  console.log(age);
+}
+
+function changeAge() {
+  age = 25;
+  console.log(age);
+}
+
+displayAge();
+changeAge();
+displayAge();
---
+++ closures/q20/2.js
@@ -0,0 +1,14 @@
+let age = 20;
+
+function displayAge() {
+  console.log(age);
+}
+
+function changeAge() {
+  age = 25;
+  console.log(age);
+}
+
+displayAge();
+changeAge();
+displayAge();
